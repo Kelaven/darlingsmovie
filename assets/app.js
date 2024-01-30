bfc59c6a3012ed3c6ba05fbaa660077e4fe40727
Kelaven <kevin.lavenant@live.fr>
2024-01-30 16:42:10 +0100

    front de la homepage avancé et carrousel fonctionnel

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,56 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+
+
+
+// ! Variables
+
+// * to make carousel on paysages
+const leftBtn = document.querySelector('.carousel__btn--left');
+const rightBtn = document.querySelector('.carousel__btn--right');
+const parents = document.querySelectorAll('.div4');
+let activeIndex = 0;
+let currentParent;
+
+// console.log(parents);
+
+// ! Functions
+
+// * to activate carousel on paysages
+clickRightBtn();
+clickLeftBtn();
+
+function clickRightBtn() {
+    rightBtn.addEventListener('click', () => {
+        activeIndex++;
+
+        if (activeIndex >= parents.length) {
+            activeIndex = 0;
+        }
+
+        currentParent = document.querySelector(`[data-status="active"]`);
+        currentParent.dataset.status = "inactive";
+
+        parents[activeIndex].dataset.status = "active";
+        // console.log(activeIndex);
+    });
+}
+
+function clickLeftBtn() {
+    leftBtn.addEventListener('click', () => {
+        activeIndex--;
+
+        if (activeIndex < 0) {
+            activeIndex = parents.length - 1;
+        }
+
+        currentParent = document.querySelector(`[data-status="active"]`);
+        currentParent.dataset.status = "inactive";
+
+        parents[activeIndex].dataset.status = "active";
+        // console.log(activeIndex);
+    });
+}
