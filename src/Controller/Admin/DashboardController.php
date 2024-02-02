c67fa3b49602068f08c6c091ebb54dbb4de4d5f4
Kelaven <kevin.lavenant@live.fr>
2024-02-02 16:40:10 +0100

    crud movie ok

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Category;
+use App\Entity\Movie;
+use App\Entity\Actor;
+
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Darlingsmovie');
+            // ->renderContentMaximized();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-icons', Category::class);
+        yield MenuItem::linkToCrud('Movies', 'fa fa-film', Movie::class);
+        yield MenuItem::linkToCrud('Actors', 'fa fa-masks-theater', Actor::class);
+    }
+}
